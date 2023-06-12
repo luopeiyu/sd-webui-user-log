d122dd47983b987e487676ec838c0b25d850b0d4
luopeiyu <luopeiyu@4399.com>
2023-06-12 17:54:43 +0800

    增加日志内容

--- scripts/userlog.py
+++ scripts/userlog.py
@@ -7,7 +7,7 @@ from modules.shared import opts, cmd_opts, state
 import modules.script_callbacks as script_callbacks
 from fastapi import FastAPI, Request
 import gradio as gr
-
+import modules
 
 user_log_request_ip = "0.0.0.0"
 user_log_cookie_user = ""
@@ -18,7 +18,7 @@ user_log_cookie_user = ""
 class PostprocessImageArgs:
     def __init__(self, image):
         self.image = image
-        
+
 class Script(scripts.Script):
     def __init__(self) -> None:
         super().__init__()
@@ -37,18 +37,32 @@ class Script(scripts.Script):
             ',', '').replace(':', ''))
         return model_name
 
+    def get_processing_tpye(self, p):
+        if type(p) == modules.processing.StableDiffusionProcessingImg2Img:
+            return "Img2Img"
+        elif type(p) == modules.processing.StableDiffusionProcessingTxt2Img:
+            return "Txt2Img"
+        else:
+            return "Unknow"
+
     def postprocess(self, p, processed, *args):
         """
         This function is called after processing ends for AlwaysVisible scripts.
         args contains all values returned by components from ui()
         """
         timeStr = time.strftime('%Y-%m-%d %H:%M:%S',
-                                time.localtime(time.time() + 8*3600)) 
+                                time.localtime(time.time() + 8*3600))
         model_name = self.get_model_name()
         ip = user_log_request_ip
         user = user_log_cookie_user
-        
-        text = timeStr + "," + ip + "," + user + "," + model_name + "\n"
+        processing_tpye = self.get_processing_tpye(p)
+
+        n_iter = str(p.n_iter)
+        batch_size = str(p.batch_size)
+
+        text = "{timeStr},{ip},{user},{processing_tpye},{n_iter},{batch_size},{model_name}\n".format(
+            timeStr=timeStr, model_name=model_name, ip=ip, user=user, processing_tpye=processing_tpye, n_iter=n_iter, batch_size=batch_size)
+
         print(text)
 
         with open(self.logfile, 'a') as f:
@@ -68,6 +82,7 @@ class Script(scripts.Script):
 
 
 
+
 def on_app_started(demo: gr.Blocks, app: FastAPI):
 
     @app.middleware("http")
@@ -77,7 +92,7 @@ def on_app_started(demo: gr.Blocks, app: FastAPI):
 
         user_log_request_ip = request.client.host
         user_log_cookie_user = "sh0000"
-        
+
         user = request.cookies.get('user')
         if user != "":
             user_log_cookie_user = user
@@ -85,6 +100,5 @@ def on_app_started(demo: gr.Blocks, app: FastAPI):
         response = await call_next(request)
         return response
 
-    
-script_callbacks.on_app_started(on_app_started)
 
+script_callbacks.on_app_started(on_app_started)
